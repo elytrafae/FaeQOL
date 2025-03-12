3b0cb532654f61e61e99f16979186fff50580d98
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-03-12 12:40:40 +0200

    Did some simple changes to this file
    I should get a proper development environment set up on this machine asap

--- Content/BuilderToggles/TorchPickupBuilderToggle.cs
+++ Content/BuilderToggles/TorchPickupBuilderToggle.cs
@@ -12,38 +12,28 @@ using Terraria.ModLoader;
 namespace FaeQOL.Content.BuilderToggles {
 
     public class TorchPickupBuilderToggle : BuilderToggle {
-		public static LocalizedText TorchPickup { get; private set; }
-		public static LocalizedText[] ColorText { get; private set; }
+		public static LocalizedText OnText { get; private set; }
+		public static LocalizedText OffText { get; private set; }
 
 		public override void SetStaticDefaults() {
-			CurrentColorText = this.GetLocalization("CurrentColor");
-			ColorText = Enumerable.Range(0, 4).Select(i => this.GetLocalization($"Color_{i}")).ToArray();
+			OnText = this.GetLocalization(nameof(OnText));
+			OffText = this.GetLocalization(nameof(OffText));
 		}
 
-		public override bool Active() => Main.LocalPlayer.HeldItem.IsAir;
+		public override bool Active() => Main.LocalPlayer.unlockedBiomeTorches;
 
-		public override int NumberOfStates => 4;
+		public override int NumberOfStates => 2;
 
 		public override string DisplayValue() {
-			return CurrentColorText.Format(ColorText[CurrentState].Value);
+			return CurrentState == 1 ? OnText : OffValue;
 		}
 
 		public override bool Draw(SpriteBatch spriteBatch, ref BuilderToggleDrawParams drawParams) {
-			Color[] colors = [Color.Red, Color.Blue, Color.Green, Color.Yellow];
-			drawParams.Color = colors[CurrentState];
+			// TODO: Add rendering
+			// CurrentState
 			return true;
 		}
 
-
-		// Right click to cycle through states backwards.
-		public override void OnRightClick() {
-			CurrentState -= 1;
-			if (CurrentState < 0) {
-				CurrentState = NumberOfStates - 1;
-			}
-
-			SoundEngine.PlaySound(SoundID.Coins);
-		}
 	}
 
 }
