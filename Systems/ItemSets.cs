7ea7b4bd3434c966ca7939e73fbe25ef5686d7d9
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-03-13 08:31:40 +0200

    More Torch Pickup stuff

--- Systems/ItemSets.cs
+++ Systems/ItemSets.cs
@@ -9,10 +9,21 @@ using Terraria.ModLoader;
 namespace FaeQOL.Systems {
     internal class ItemSets : ModSystem {
 
-        private static bool[] IsKey = null;// ItemID.Sets.Factory.CreateBoolSet(ItemID.GoldenKey, ItemID.ShadowKey, ItemID.TempleKey, ItemID.JungleKey, ItemID.FrozenKey, ItemID.HallowedKey, ItemID.CorruptionKey, ItemID.CrimsonKey, ItemID.DungeonDesertKey); 
+        private static bool[] IsKey = null;
+        public static bool[] IsTorchGodTorch = null;
+        public static bool[] IsTorchGodCampfire = null;
 
         public override void Load() {
             IsKey = ItemID.Sets.Factory.CreateBoolSet(ItemID.GoldenKey, ItemID.ShadowKey, ItemID.TempleKey, ItemID.JungleKey, ItemID.FrozenKey, ItemID.HallowedKey, ItemID.CorruptionKey, ItemID.CrimsonKey, ItemID.DungeonDesertKey);
+            IsTorchGodTorch = ItemID.Sets.Factory.CreateBoolSet(ItemID.IceTorch, ItemID.DesertTorch, ItemID.JungleTorch, ItemID.HallowedTorch, ItemID.CorruptTorch, ItemID.CrimsonTorch, ItemID.CursedTorch, ItemID.IchorTorch, ItemID.CoralTorch, ItemID.MushroomTorch, ItemID.BoneTorch, ItemID.DemonTorch, ItemID.ShimmerTorch);
+            IsTorchGodCampfire = ItemID.Sets.Factory.CreateBoolSet(ItemID.FrozenCampfire, ItemID.DesertCampfire, ItemID.JungleCampfire, ItemID.HallowedCampfire, ItemID.CorruptCampfire, ItemID.CrimsonCampfire, ItemID.CursedCampfire, ItemID.IchorCampfire, ItemID.CoralCampfire, ItemID.MushroomCampfire, ItemID.BoneCampfire, ItemID.DemonCampfire, ItemID.ShimmerCampfire);
+        }
+
+        public override void PostSetupContent() {
+            foreach (ModBiome biome in ModContent.GetContent<ModBiome>()) {
+                IsTorchGodTorch[biome.BiomeTorchItemType] = true;
+                IsTorchGodCampfire[biome.BiomeCampfireItemType] = true;
+            }
         }
 
         public static bool IsItemKey(int type) { 
